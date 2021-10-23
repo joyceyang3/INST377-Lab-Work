be07d089314a250136184a8fb26f19b91388e1a4
joyceyang3 <jyang113@umd.edu>
2021-10-22 23:02:05 -0400

    Edited script

---
+++ client/lab_4/script.js
@@ -0,0 +1,27 @@
+let current = first;
+const first = document.querySelectorAll(".carousel-item first-item--visible");
+const last = document.querySelectorAll("carousel-item last-item");
+
+document.querySelector("previous-button").onclick = (e) => {
+    if (current.previousElementSibiling) {
+        current.classlist.remove("visible");
+        current.previousElementSibling.classList.add("visible");
+        current = current.previousElementSibiling;
+    } else {
+        current.classList.remove("visible");
+        first.classList.add("visible");
+        current = last;
+    }
+};
+
+document.querySelector("next-button").onclick = (e) => {
+    if (current.nextElementSibiling) {
+        current.classlist.remove("visible");
+        current.nextElementSibling.classList.add("visible");
+        current = current.nextElementSibiling;
+    } else {
+        current.classList.remove("visible");
+        first.classList.add("visible");
+        current = first;
+    }
+};
