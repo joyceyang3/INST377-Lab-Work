dd0276b033ebf8e6d93abb48f954a0e18da9b727
joyceyang3 <jyang113@umd.edu>
2021-12-03 21:39:57 -0500

    doodle js

--- client/doodle/app.js
+++ client/doodle/app.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+
 
     function createDoodler() {
     grid.appendChild(doodler)
     doodler.classLast.add('doodler')
-     
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+     j
     }
-    createDoodler()
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+            createPlatforms()
+        }
+    }
+    //attach to buttom
+    start()
 })
